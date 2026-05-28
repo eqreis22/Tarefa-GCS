ef9a7a3ff32308af9f577fee4229bbd44e54a44c
Erllison Reis <erllisonreis22@yahoo.com>
2026-05-27 23:05:05 -0400

    commit das funcoes
    commitando as funcoes

---
+++ Tarefa_CGS/src/termometro.java
@@ -0,0 +1,18 @@
+package com.projeto.jenkins;
+
+public class ConversorTemperatura {
+/**
+* Converte uma temperatura de Celsius para Fahrenheit.
+* Formula: (C * 9/5) + 32
+*/
+    public double celsiusParaFahrenheit(double celsius) {
+        return (celsius * 9.0 / 5.0) + 32.0;
+    }
+/**
+* Converte uma temperatura de Fahrenheit para Celsius.
+* Formula: (F - 32) * 5/9
+*/
+    public double fahrenheitParaCelsius(double fahrenheit) {
+        return (fahrenheit - 32.0) * 5.0 / 9.0;
+    }
+}
